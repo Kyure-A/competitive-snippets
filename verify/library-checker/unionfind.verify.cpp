854f60cebf86996a7ccfe099b45c7a1f6d7e1496
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-05-18 00:54:59 +0900

    [Added] verify (unionfind)

---
+++ verify/library-checker/unionfind.verify.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+#include "../../kyre/UnionFind.hpp"
+#include <bits/stdc++.h>
+using namespace std;
+
+signed main ()
+{
+  int n, q; cin >> n >> q;
+
+  kyre::UnionFind tree(n);
+  
+  while (q--)
+    {
+      int t, u, v; cin >> t >> u >> v;
+      u--; v--;
+
+      if (t == 0)
+	{
+	  tree.merge(u, v);
+	}
+      
+      if (t == 1)
+	{
+	  if (tree.same(u, v)) cout << 1 << endl;
+	  else cout << 0 << endl;
+	}
+    }
+}
